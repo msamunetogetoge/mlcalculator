84d9ac6df971be9ae508100d8d86253128a17fd2
masamune <kenjiii534@gmail.com>
2020-10-13 18:55:29 +0900

    modified:   manage.py 	modified:   settings.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MMLCALCULATOR.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MLkit.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
