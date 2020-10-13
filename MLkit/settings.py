1758665eeb0a73393563cbabc9b6fd961c20588a
masamune <kenjiii534@gmail.com>
2020-10-14 01:57:07 +0900

    settings.py ENGINE changes postgresql

--- MLkit/settings.py
+++ MLkit/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'MLkit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mmlcalculator',
         'USER': 'masamune',
         'PASSWORD': 'kenji0121',
